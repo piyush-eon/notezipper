448c41b107a774df857882e718e836da628999bb
Piyush55dude <piyushagarwalvo@gmail.com>
2020-12-03 20:22:46 +0530

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
+import path from "path";
 
 import noteRoutes from "./routes/noteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -15,12 +16,22 @@ const app = express(); // main thing
 
 app.use(express.json()); // to accept json data
 
-app.get("/", (req, res) => {
-  res.send("API is running..");
-});
-
 app.use("/api/notes", noteRoutes);
 app.use("/api/users", userRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 // Error Handling middlewares
 app.use(notFound);
