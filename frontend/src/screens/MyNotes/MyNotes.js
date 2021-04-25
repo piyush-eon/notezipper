f26decd6796834a8483231e68f9127c9c4a251a0
Piyush55dude <piyushagarwalvo@gmail.com>
2021-04-25 18:18:47 +0530

    bug fixes

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { Badge, Button, Card } from "react-bootstrap";
+import React, { useEffect } from "react";
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
 import MainScreen from "../../components/MainScreen";
 import { Link } from "react-router-dom";
-import NoteModal from "../../components/NoteModal";
+// import NoteModal from "../../components/NoteModal";
 import ReactMarkdown from "react-markdown";
 
 import { useDispatch, useSelector } from "react-redux";
@@ -11,8 +11,8 @@ import Loading from "../../components/Loading";
 import ErrorMessage from "../../components/ErrorMessage";
 
 function MyNotes({ history, search }) {
-  const [noteModal, setNoteModal] = useState("");
-  const [modalShow, setModalShow] = useState(false);
+  // const [noteModal, setNoteModal] = useState("");
+  // const [modalShow, setModalShow] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -53,10 +53,10 @@ function MyNotes({ history, search }) {
     successUpdate,
   ]);
 
-  const ModelShow = (note) => {
-    setModalShow(true);
-    setNoteModal(note);
-  };
+  // const ModelShow = (note) => {
+  //   setModalShow(true);
+  //   setNoteModal(note);
+  // };
 
   const deleteHandler = (id) => {
     if (window.confirm("Are you sure?")) {
@@ -85,53 +85,66 @@ function MyNotes({ history, search }) {
           )
           .reverse()
           .map((note) => (
-            <Card style={{ margin: 10 }} key={note._id}>
-              <Card.Header style={{ display: "flex" }}>
-                <span
-                  onClick={() => ModelShow(note)}
-                  style={{
-                    color: "black",
-                    textDecoration: "none",
-                    flex: 1,
-                    cursor: "pointer",
-                    alignSelf: "center",
-                  }}
-                >
-                  {note.title}
-                </span>
-
-                <div>
-                  <Button href={`/note/${note._id}`}>Edit</Button>
-                  <Button
-                    variant="danger"
-                    className="mx-2"
-                    onClick={() => deleteHandler(note._id)}
+            <Accordion>
+              <Card style={{ margin: 10 }} key={note._id}>
+                <Card.Header style={{ display: "flex" }}>
+                  <span
+                    // onClick={() => ModelShow(note)}
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18,
+                    }}
                   >
-                    Delete
-                  </Button>
-                </div>
-              </Card.Header>
-              <Card.Body>
-                <h4>
-                  <Badge variant="success">Category - {note.category}</Badge>
-                </h4>
-                <blockquote className="blockquote mb-0">
-                  <ReactMarkdown>{note.content}</ReactMarkdown>
-                  <footer className="blockquote-footer">
-                    Created on{" "}
-                    <cite title="Source Title">
-                      {note.createdAt.substring(0, 10)}
-                    </cite>
-                  </footer>
-                </blockquote>
-              </Card.Body>
-            </Card>
+                    <Accordion.Toggle
+                      as={Card.Text}
+                      variant="link"
+                      eventKey="0"
+                    >
+                      {note.title}
+                    </Accordion.Toggle>
+                  </span>
+
+                  <div>
+                    <Button href={`/note/${note._id}`}>Edit</Button>
+                    <Button
+                      variant="danger"
+                      className="mx-2"
+                      onClick={() => deleteHandler(note._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <h4>
+                      <Badge variant="success">
+                        Category - {note.category}
+                      </Badge>
+                    </h4>
+                    <blockquote className="blockquote mb-0">
+                      <ReactMarkdown>{note.content}</ReactMarkdown>
+                      <footer className="blockquote-footer">
+                        Created on{" "}
+                        <cite title="Source Title">
+                          {note.createdAt.substring(0, 10)}
+                        </cite>
+                      </footer>
+                    </blockquote>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
           ))}
-      <NoteModal
+      {/* <NoteModal
         show={modalShow}
         noteModal={noteModal}
         onHide={() => setModalShow(false)}
-      />
+      /> */}
     </MainScreen>
   );
 }
